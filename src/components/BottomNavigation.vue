833861eb4cfe67bab7343924662b1c83866344d9
Jaka Škerjanc <100130505+jakaskerjanc@users.noreply.github.com>
2023-07-01 12:10:54 +0200

    Tab rename to slovenian

--- src/components/BottomNavigation.vue
+++ src/components/BottomNavigation.vue
@@ -1,20 +1,20 @@
 <template>
   <v-bottom-navigation
     :value="activeTab"
-    bg-color="red"
+    color="red"
     @update:model-value="$emit('update:activeTab', $event)"
   >
     <v-btn value="map">
       <v-icon>mdi-map</v-icon>
-      <span>Map</span>
+      <span>Zemljevid</span>
     </v-btn>
     <v-btn value="list">
       <v-icon>mdi-list-box</v-icon>
-      <span>List</span>
+      <span>Seznam</span>
     </v-btn>
     <v-btn value="search">
       <v-icon>mdi-magnify</v-icon>
-      <span>Search</span>
+      <span>Iskanje</span>
     </v-btn>
   </v-bottom-navigation>
 </template>
